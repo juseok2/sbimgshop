12f87bc39c31f0fb273fa765e8d944c0a5fec033
juseok2 <wntjrwnstjr@naver.com>
2024-11-06 15:25:44 +0900

    홈 화면 구현

---
+++ src/main/java/com/mysite/sbimgshop2/common/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.mysite.sbimgshop2.common.controller;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"/", "/home"})
+    public String home(Model model) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = now.format(formatter);
+        model.addAttribute("date", formattedDate);
+        return "common/home";
+    }
+}
